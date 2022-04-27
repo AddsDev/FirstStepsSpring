2b4d593f4cd6c52ef5f7b712c40edfdd4e36da86
Adrian Ruiz <adrianruiz.981031@gmail.com>
2022-04-26 21:11:08 -0500

    Update HomeController.java
    Se modifica el request mapping del controlador, se crea otro endpoint para el ejemplo de las diferentes respuestas del api

--- demoSpring/src/main/java/com/example/demo/controllers/HomeController.java
+++ demoSpring/src/main/java/com/example/demo/controllers/HomeController.java
@@ -3,15 +3,27 @@ package com.example.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.xml.ws.Response;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping(path = "/api")
 public class HomeController {
 
-    @RequestMapping(value = "/home", method = RequestMethod.GET)//value o path
-    @GetMapping(value = "/home")
-    public String index(){
+    //@RequestMapping(path = "/", method = RequestMethod.GET)//value o path
+    @GetMapping(path = {"/saludo","/bye"})
+    public HashMap<String, String> home(){
 
-        return "Bienvenido al Home";
+        HashMap<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        map.put("foo", "bar");
+        map.put("aa", "bb");
+        return map;
+    }
+    @GetMapping(path = "/name")
+    public Object name(){
+        return "Name";
     }
 }
